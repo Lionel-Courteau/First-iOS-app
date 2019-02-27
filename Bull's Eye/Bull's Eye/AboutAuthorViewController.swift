531a97392dd0647c55ad9bc10d8f4cef7dc449f7
Lionel Courteau <42822395+Lionel-Courteau@users.noreply.github.com>
2019-02-27 09:38:54 -1000

    Add files via upload

---
+++ Bull's Eye/Bull's Eye/AboutAuthorViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutAuthorViewController.swift
+//  Bull's Eye
+//
+//  Created by Student on 2/25/19.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
